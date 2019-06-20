5f4e543bc40f0edde047f4b6882223bf65c49949
Vinny <vinny@vinny.nomad.utk.edu>
2019-06-20 15:38:49 -0400

    file move -> parameter updated

--- example.R
+++ example.R
@@ -1,8 +1,12 @@
-data <- read.csv("Large_Hospital_New2004.csv", header = TRUE)
+library(data.table)
+library(reshape2)
+library(cluster)
+
+data <- read.csv("example data/Large_Hospital_New2004.csv", header = TRUE)
 
 head(data)
 
-my_exp <- create_grouping_exp(file = "Large_Hospital_New2004.csv",
+my_exp <- create_grouping_exp(file = "example data/Large_Hospital_New2004.csv",
                               num_of_groups = 10)
 
 class(my_exp)
@@ -12,7 +16,7 @@ head(my_exp[[1]])
 #the names have long IDF style names. Here is a small cut from the 
 #output for the second element (the design matrix)
 small_design <- as.matrix(my_exp[[2]][1:8, 101:105])
-colnames(small_desgin)
+colnames(small_design)
 
 #remove the names to make the design matrix readable
 colnames(small_design) <- NULL
