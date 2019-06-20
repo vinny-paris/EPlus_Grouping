98cbda103a7b565aea286d87db86721e8dd74fc6
Vinny <vinny@vinny.nomad.utk.edu>
2019-06-20 11:05:17 -0400

    clarified a parameter by name

--- final_function.R
+++ final_function.R
@@ -1,5 +1,5 @@
-create_grouping_exp <- function(file, group_size = 10){
-  m <- make_groups(file, group_size = group_size) 
+create_grouping_exp <- function(file, num_of_groups = 10){
+  m <- make_groups(file, group_size = num_of_groups) 
   n <- make_design(m) 
   return(n)
 }
