6909618a8856744e1e2f9e301ef1f542c691221e
Vinny <vinny@vinny.nomad.utk.edu>
2019-06-20 11:37:13 -0400

    better naming of output

--- final_function.R
+++ final_function.R
@@ -1,6 +1,13 @@
+#Pass this function a file of the formate that parameter 
+#information is given in from Joshua New, then pass the 
+#number of groups you would like (default is 10). Larger
+#group number means more runs but more accuracey in reduced
+#effects cancelling.
+
 create_grouping_exp <- function(file, num_of_groups = 10){
   m <- make_groups(file, group_size = num_of_groups) 
-  n <- make_design(m) 
-  return(n)
+  n <- make_design(m)
+  o <- list(m, n)
+  return(o)
 }
   
